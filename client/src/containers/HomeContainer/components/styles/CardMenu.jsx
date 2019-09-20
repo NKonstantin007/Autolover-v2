6620cad3ba46e1a7778917e822888a228a55a81e
kostya007 <kostya.1998.nosarev@mail.ru>
2019-09-20 14:29:01 +0700

    Create a menu containt Card component

---
+++ client/src/containers/HomeContainer/components/styles/CardMenu.jsx
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+import {keyframes} from '@emotion/core';
+import {Card} from 'reactstrap';
+
+import {PrimaryColor} from '../../../../styles/colors';
+
+export const CardMenuWrapper = styled.div`
+    padding: 70px 10% 40px 10%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    div {
+        width: 100%;
+        margin-left: 10px;
+
+        &:first-of-type {
+            margin-right: 0;
+        }
+    }
+`;
+
+const rotate = keyframes`
+	from {
+        transform: rotate(0deg);
+        opacity: 1;
+    }
+	to{
+        transform: rotate(-15deg);
+        opacity: 0.9;
+    }
+`;
+
+export const CardMenuItem = styled(Card)`
+
+    &:hover {
+        animation-name: ${rotate};
+        animation-duration: 0.8s;
+        animation-timing-function: ease-in;
+        animation-delay: 0.5s;
+        animation-fill-mode: forwards;
+    }
+`;
+
+export const CardMenuTitle = styled.h5`
+    margin-bottom: 20px;
+    text-align: center;
+    letter-spacing: 3px;
+`;
+
+export const CardMenuSubtitle = styled.h3`
+    margin-bottom: 20px;
+    text-align: center;
+`;
+
+export const CardMenuImg = styled.img`
+    border-radius: 5px;
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+    border: 2px solid ${PrimaryColor};  
+`;
+
+export const CardMenuText = styled.p`
+    padding-left: 5px;
+    padding-right: 5px;
+`;
--- client/src/containers/HomeContainer/index.jsx
+++ client/src/containers/HomeContainer/index.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+ 
+import {
+    CardMenuWrapper,
+    CardMenuItem,
+    CardMenuTitle,
+    CardMenuSubtitle,
+    CardMenuImg,
+    CardMenuText
+} from './components/styles/CardMenu';
 
 import Jumbotron from './components/Jumbotron';
 
@@ -6,6 +16,52 @@ const HomeContainer = () => {
     return (
         <div>
             <Jumbotron />
+            <CardMenuWrapper>
+                <div>
+                    <CardMenuTitle>Все об автомобилях</CardMenuTitle>
+                    <CardMenuItem>
+                        <Link to="/allcars">
+                            <CardMenuImg src="/images/all-about-cars.jpg" />
+                            <CardMenuSubtitle>Авто — лучший друг человека</CardMenuSubtitle>
+                        </Link>
+                        <CardMenuText>
+                            Автомобили являются самым величайшим достижением высоких технологий. 
+                            В одной металлической коробочке скрывается плод стараний сотен дизайнеров, 
+                            инженеров, история, мощь и вкус
+                        </CardMenuText>
+                    </CardMenuItem>
+                </div>
+
+                <div>
+                    <CardMenuTitle>Интересные факты</CardMenuTitle>
+                    <CardMenuItem>
+                        <Link to="/facts">
+                            <CardMenuImg src="/images/facts-about-car.jpeg" />
+                            <CardMenuSubtitle>Интересные факты об авто</CardMenuSubtitle>
+                        </Link>
+                        <CardMenuText>
+                            Автомобили такие, какими мы привыкли их видеть на улицах городов, 
+                            были  сделаны всего несколько десятков лет назад. А история изобретения
+                            автомобиля насчитывает несколько сотен лет
+                        </CardMenuText>
+                    </CardMenuItem>
+                </div>
+
+                <div>
+                    <CardMenuTitle>Обзоры автомобилей</CardMenuTitle>
+                    <CardMenuItem>
+                        <Link to="/facts">
+                            <CardMenuImg src="/images/car-reviews.jpg" />
+                            <CardMenuSubtitle>Самые популярные марки</CardMenuSubtitle>
+                        </Link>
+                        <CardMenuText>
+                            На вопрос «какая самая лучшая марка автомобиля?» нет ответа! 
+                            У каждого автолюбителя свои вкусы, предпочтения и восможности. 
+                            А какой автомобиль нравится вам?
+                        </CardMenuText>
+                    </CardMenuItem>
+                </div>
+            </CardMenuWrapper>
         </div>
     );
 }
