b7e62daa741513677695e56cfd3994f4f9c7884e
kostya007 <kostya.1998.nosarev@mail.ru>
2019-10-18 18:48:40 +0700

    Add validation of sign in form on client

--- client/src/containers/SignInContainer/components/SignInForm.jsx
+++ client/src/containers/SignInContainer/components/SignInForm.jsx
@@ -6,10 +6,48 @@ import Input from '../../../components/formElements/Input'
 import Label from '../../../components/formElements/styles/Label';
 import Button from '../../../styles/components/Button';
 
+const validate = (values) => {
+    const charactersPattern = /[^0-9a-z_-]/i;   // RegExp to validate the input of valid characters
+    const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i // RegExp to validate email
+    const errors = {}
+    //--- Email check ---
+    // check email for emptiness
+    if (!values.email) {
+        errors.email = 'Введите email';
+    }
+    // check email for emptiness with space
+    else if (values.email.trim() === '') {
+        errors.name = 'Введите email';
+    }
+    // check email for compliance with the pattern
+    else if (!emailPattern.test(values.email.trim())) {
+        errors.email = 'Email не соответствует шаблону';
+    }
+
+    //--- Password check ---
+    // check password for emptiness
+    if (!values.password) {
+        errors.password = 'Введите пароль';
+    }
+    // check password for emptiness with space
+     else if (values.password.trim() === '') {
+        errors.name = 'Введите пароль';
+    }
+    // check password min length 
+    else if (values.password.length < 6) {
+        errors.password = 'Длинна пароля должна быть не меньше 6 символов';
+    }
+    // check password for compliance with the pattern
+    else if(!charactersPattern.test(values.password.trim())) {
+        errors.name='Пароль содержит недопустимые символы';
+    }
+    return errors
+  }
+
 const SignInForm = (props) => {
     const {handleSubmit} = props;
     return (
-        <StyledSignInForm onSubmit={handleSubmit}>
+        <StyledSignInForm noValidate onSubmit={handleSubmit}>
             <div>
                 <Label>Email</Label>
                 <Field name="email" type="email" component={Input} />
@@ -23,6 +61,7 @@ const SignInForm = (props) => {
     );
 }
 
-export default reduxForm(
-    { form: 'SignInForm'}
-)(SignInForm);
+export default reduxForm({ 
+    form: 'SignInForm',
+    validate
+})(SignInForm);
